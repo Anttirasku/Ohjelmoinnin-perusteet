89abf4ee40b1c5c6e96c87876055a8b5f969a061
Antti Rasku <ae5556@student.jamk.fi>
2023-09-22 02:17:51 +0300

    Tehtävän L03T05 palautus.

---
+++ LAB03/TehtäväL03T05.py
@@ -0,0 +1,26 @@
+luku1=int(input("Anna 1.luku: "))
+if luku1>0:
+    numero1=luku1
+elif luku1<=0:
+    numero1=luku1*1
+luku2=int(input("Anna 2.luku: "))
+if luku2>0:
+    numero2=luku2
+elif luku2<=0:
+    numero2=luku2*1
+luku3=int(input("Anna 3.luku: "))
+if luku3>0:
+    numero3=luku3
+elif luku3<=0:
+    numero3=luku3*1
+luku4=int(input("Anna 4.luku: "))
+if luku4>0:
+    numero4=luku4
+elif luku4<=0:
+    numero4=luku4*1
+luku5=int(input("Anna 5.luku: "))
+if luku5>0:
+    numero5=luku5
+elif luku5<=0:
+    numero5=luku5*1
+print(numero1+numero2+numero3+numero4+numero5)
