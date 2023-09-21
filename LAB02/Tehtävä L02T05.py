99b3050d4ba74689df7f52412602946b62dac455
Antti Rasku <ae5556@student.jamk.fi>
2023-09-21 19:35:25 +0300

    Lab02 tehtävän L02T05 palautus.

--- LAB02/Tehtävä L02T05.py
+++ LAB02/Tehtävä L02T05.py
@@ -1,1 +1,5 @@
-string="jeesus"
+firstname=input("Syötä etunimesi  ")
+first_letter=firstname[0:1]
+lettercount=len(firstname)
+print("Nimessäsi on",lettercount,"kirjainta.")
+print(first_letter*lettercount)
