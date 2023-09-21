ddddbe7c30e62e836cbc015b0da7e49099e1704e
Antti Rasku <ae5556@student.jamk.fi>
2023-09-21 19:03:43 +0300

    Tehtävän L03T01 palautus

---
+++ LAB03/Tehtävä L03T01.py
@@ -0,0 +1,7 @@
+luku1=int(input("Anna kokonaisluku  "))
+luku2=int(input("Anna toinen kokonaisluku  "))
+if luku1<luku2:
+    print("Pienempi luku on ",luku1)
+elif luku1>luku2: print("Pienempi luku on", luku2)
+elif luku1==luku2:
+    print("Luvut ovat yhtäsuuria!")
